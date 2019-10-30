b707b11886f2a03c82c80eb9d4a4f5ad4453e073
Marck <18088281+Marck@users.noreply.github.com>
2019-10-30 11:08:00 +0100

    Added docker-compose.yml, added dev config and added Gambit game mode (#1)
    * Added docker-compose
    * Added Gambit gamemode
    * Create template file for development config

---
+++ commands/gambit.js
@@ -0,0 +1,64 @@
+const Moment = module.require('moment');
+const Command = require('../lib/Command');
+const GameMode = new (require('../lib/GameMode'));
+
+/**
+ * Gambit
+ * @class
+ * @extends Command
+ */
+class Gambit extends Command {
+    /**
+     * Constructor
+     * @param {Discord.Client} client
+     */
+    constructor(client) {
+        super(client, {
+            name: "gambit",
+            category: "activity",
+            cooldown: 1000,
+            permission: "READ_MESSAGE"
+        });
+
+        this.data = GameMode.Gambit();
+
+        // Set usage
+        this.help.description = 'Hoe maak ik een gambit activiteit aan:';
+        this.help.usage = `${this.client.config.prefix}gambit <MODE> <DATUM> <TIJD> [NOTE]
+
+<MODE>      Moet een geldige gambit mode zijn. Geldige modes zijn:
+            ${Object.keys(this.data.mode).join(', ')}.
+
+<DATUM>     Moet een geldige datum zijn. Datum formaat moet zijn: DD-MM-YYYY.
+            Voorbeeld: ${Moment().format('DD-MM-YYYY')} voor ${Moment().format('LL')}.
+
+<TIJD>      Moet een geldige tijd zijn. Tijd formaat moet zijn: HH:mm.
+            Voorbeeld: ${Moment().format('HH:mm')} voor de huidige tijd.
+
+[NOTE]      Deze optie kan gebruikt worden om een notitie toe te voegen aan de activiteit.
+            Dit is een optioneel veld.`;
+    }
+
+    /**
+     *
+     * @param {Discord.Message} message
+     * @param {Array} args
+     * @returns {boolean}
+     */
+    run(message, args) {
+        if (this.validateInput('gambit', args, this.data) === true) {
+
+            if (args[0] === 'help') { this.sendHelp(); return true; }
+
+            let embed = this.createActivityEmbed('Gambit', args, this.data);
+
+            super.respond(embed).then(async embedActivity => {
+                await embedActivity.react('✅');
+                await embedActivity.react('❎');
+                await embedActivity.react('🕒');
+            });
+        }
+    }
+}
+
+module.exports = Gambit;
